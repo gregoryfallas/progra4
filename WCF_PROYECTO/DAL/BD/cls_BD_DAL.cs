a586232f14a4cda180f1ba04c4926346130d71ac
falla <falla@ANON>
2020-03-31 22:57:31 -0600

    Start project

---
+++ WCF_PROYECTO/DAL/BD/cls_BD_DAL.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace DAL.BD
+{
+    public class cls_BD_DAL
+    {
+        #region Variables Privadas
+
+            private string _sMsjError, _sCadenaConec;
+
+        #endregion
+
+        #region Variables Públicas
+
+            public string sCadenaConec
+            {
+                get
+                {
+                    return _sCadenaConec;
+                }
+
+                set
+                {
+                    _sCadenaConec = value;
+                }
+            }
+
+            public string sMsjError
+            {
+                get
+                {
+                    return _sMsjError;
+                }
+
+                set
+                {
+                    _sMsjError = value;
+                }
+            }
+
+            public SqlConnection Obj_SQL_CNX;
+
+            public SqlDataAdapter Obj_SQL_DAP;
+
+            public SqlCommand Obj_SQL_CMD;
+
+        #endregion
+
+    }
+}
---
+++ WCF_PROYECTO/WCF_Proyecto_SVC/Interface/IBD.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using System.Data;
+
+namespace WCF_Proyecto_SVC.Interface
+{        
+    [ServiceContract]
+    public interface IBD
+    {
+        [OperationContract]
+        DataTable CrearDTParametros();
+
+
+        [OperationContract]
+        DataTable ListarDatos(string sNombreSP, ref string sMsjError);
+                       
+
+        [OperationContract]
+        string Ins_Mod_Del_Datos(string sNombreSP, bool bBandera, DataTable DT_Parametros, ref string sMsjError);
+              
+    }
+}
