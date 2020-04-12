142ccacb6e304e064f25c69f34b307544d7bfd7b
ksibajav <ksibajav@gmail.com>
2020-04-12 14:54:26 -0600

    Cambio del WCF y acciones de la tabla tipos empleados

---
+++ WCF_PROYECTO/SVC_Aeropuerto/Interfaces/IBD.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.ServiceModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SVC_Aeropuerto.Interfaces
+{
+    [ServiceContract]
+    interface IBD
+    {
+        [OperationContract]
+        DataTable CrearDTParametros();
+
+        [OperationContract]
+        DataTable ListarFiltrarDatos(string sNombreSP, DataTable DT_Parametros, ref string sMsjError);
+
+        [OperationContract]
+        String Ins_Mod_Eli_Datos(string sNombreSP, bool bBandera, DataTable DT_Parametros, ref string sMsjError);
+    }
+}
