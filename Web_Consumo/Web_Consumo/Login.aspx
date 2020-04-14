2ea0ae62402a1a40a9c2345781879abe160c6dc0
Wesly Quesada <wquesada@mdgdevelopers.com>
2020-04-14 01:32:10 -0600

    Login (diseño y logica)

--- Web_Consumo/Web_Consumo/Login.aspx
+++ Web_Consumo/Web_Consumo/Login.aspx
@@ -4,12 +4,21 @@
 
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head runat="server">
-    <title></title>
+    <title>Login</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <link href=".\css\login.css" rel="stylesheet" />
 </head>
 <body>
-    <form id="form1" runat="server">
-        <div>
+        <div class="loginbox">
+            <img src=".\img\destination\7.png" alt="Login" class="user" />
+            <h2> Inicio de Sesion </h2>
+            <form runat="server">
+                <asp:Label Text="Usuario" CssClass="lblemail" runat="server" />
+                <asp:TextBox ID="txt_Username" runat="server" CssClass="txtemail" placeholder="Nombre de Usuario"/>
+                <asp:Label Text="Contraseña" CssClass="lblpass" runat="server" />
+                <asp:TextBox ID="txt_Password" runat="server" CssClass="txtpass" placeholder="Contraseña"/>
+                <asp:Button ID="btn_Login" Text="Login" runat="server" CssClass="btnsubmit" OnClick="btn_Login_Click"/>
+            </form>
         </div>
-    </form>
 </body>
 </html>
--- Web_Consumo/Web_Consumo/Login.aspx.cs
+++ Web_Consumo/Web_Consumo/Login.aspx.cs
@@ -11,7 +11,48 @@ namespace Web_Consumo
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            CargarDatos();
+        }
 
+        private void CargarDatos()
+        {
+            #region VARIABLES LOCALES
+
+            DataTable dt = new DataTable();
+            DataTable dtParametros = new DataTable();
+            WCF_BD.BDClient Obj_WCF_BD = new WCF_BD.BDClient();
+
+            string sNombSP = string.Empty;
+            string sMsjError = string.Empty;
+
+            #endregion
+
+            //ESTE PROCESO  REALIZA EL FILTRADO
+            dtParametros = Obj_WCF_BD.CrearDTParametros();
+            dtParametros.Rows.Add("@username", "1", txt_Username.Text.Trim());
+            dtParametros.Rows.Add("@password", "1", txt_Password.Text.Trim());
+            sNombSP = "SP_Filtrar_Login";
+
+            dt = Obj_WCF_BD.ListarFiltrarDatos(sNombSP, dtParametros, ref sMsjError);
+
+            DGV_DATOS.DataSource = null;
+            DGV_DATOS.DataSource = dt;
+            DGV_DATOS.DataBind();
+
+            if(dt.Rows.Count == 0)
+            {
+                ScriptManager.RegisterStartupScript(this, GetType(), "showalert", "alert('Usuario o contraseña Incorrectas');", true);
+            }
+            else
+            {
+                Response.Redirect("index.aspx");
+            }
+           
+        }
+
+        protected void btn_Login_Click(object sender, EventArgs e)
+        {
+            CargarDatos();
         }
     }
 }
